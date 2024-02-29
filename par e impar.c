22b0c35e824e2d83c661ccf2640ef39007189dbe
Gabriel Magnago <133276359+GabrielMagnago18@users.noreply.github.com>
2024-02-29 14:44:15 -0300

    29/02/2024

--- par e impar.c
+++ par e impar.c
@@ -1,14 +1,8 @@
 /*
 Escreva um programa que leia doze números do tipo inteiro digitados
-pelo usuário. 
-
-Separe esses números em pares e ímpares,
-
-os armazenem em dois outros vetores chamados vetpar e vetimpar.
-
-Em seguida, o programa deve apresentar os resultados na tela.
-
-
+pelo usuário. Separe esses números em pares e ímpares, os armazenem
+em dois outros vetores chamados vetpar e vetimpar. Em seguida, o
+programa deve apresentar os resultados na tela.
 */
 
 
